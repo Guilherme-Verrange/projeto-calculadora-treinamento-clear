9918184bf2804ebb75fddd58c69c5940bc32fa36
Guilherme-Verrange <verrangeguilherme@gmail.com>
2022-09-14 18:33:12 -0300

    The date and time have been added with setInterval, this way the DOM will be updated in real time

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,1 +1,82 @@
-console.log("Testando");
+class CalcController{
+
+    constructor(){
+
+        this._locale = "pt-BR";
+        this._displayEl = document.querySelector("#display");
+        this._displayDateElement = document.querySelector("#data");
+        this._displayTimeElement = document.querySelector("#hora");
+        this._currentDate;//Pega a data e hora atual
+        this.initialize();
+        this._initButtonsEvent();
+
+    }
+
+    initialize(){
+
+        this.setdisplayDateTime();
+
+        setInterval(()=>{
+
+            this.setdisplayDateTime();
+
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            //Pega o valor de displayDate em get e converte na data com toLocale
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+            //Pega o valor de displayTime em get e converte na data com toLocale
+
+        }, 1000);
+        
+    }
+
+    initButtonsEvent(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #party > g");
+
+        buttons.addEventListener('click', e =>{
+
+
+        });
+    }
+
+    setdisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime(){
+        return this._displayTimeElement.innerHTML;
+    }
+
+    set displayTime(value){
+        return this._displayTimeElement.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._displayDateElement.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._displayDateElement.innerHTML = value;
+    }
+
+    get displayCalc(){
+        return this._displayEl.innerHTML;
+    }
+
+    set displayCalc(valor){
+        this._displayEl.innerHTML =  value;
+    }
+
+    get currentDate(){
+        return new Date();
+
+    }
+
+    set currentDate(valor){
+
+        return this.currentDate = value;
+    }
+
+}
