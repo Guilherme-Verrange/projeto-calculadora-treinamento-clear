c1c1fa13f8ada54219a1428387efecfa7508e1ca
Guilherme-Verrange <verrangeguilherme@gmail.com>
2022-09-16 10:12:44 -0300

    Fixed the bug where the equals button (=) when clicking again after an operation did not enter the last value and did not make an equation.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,10 @@
 class CalcController {
   constructor() {
 
+
+
+    this._lastOperator = '';
+    this._lastNumber = '0';
     this._operation = [];
     this._locale = "pt-BR";
     this._displayEl = document.querySelector("#display");
@@ -70,24 +74,50 @@ class CalcController {
 
 
         this.calc();
-
-        console.log(this._operation);
     }
 
+  }
+
+
+  getResult(){
+
+    return eval(this._operation.join(""));
+    
   }
 
   calc(){
 
     let last = '';
 
+    this._lastOperator = this.getLastItem();
+
+    if (this._operation.length < 3){
+
+      let firstItem = this._operation[0];
+      this._operation = [firstItem, this._lastOperator, this._lastNumber];
+    
+  }
+    
+
     if (this._operation.length > 3){
 
       last = this._operation.pop();
 
+      this._lastNumber = this.getResult();
+
+    } else  if (this._operation.length == 3){
+
+      this._lastNumber = this.getLastItem(false);
+
     }
+    
+   
+
+    console.log('this._lastOperator', this._lastOperator);
+    console.log('this._lastNumber', this._lastNumber);
 
 
-    let result = eval(this._operation.join(""));
+    let result = this.getResult();
 
     if (last == '%'){
 
@@ -107,18 +137,33 @@ class CalcController {
 
   }
 
-  setLastNumberToDisplay(){
+  getLastItem(isOperator = true){
 
-    let lastNumber;
+    let lastItem;
 
     for (let i = this._operation.length-1; i >= 0; i--){
 
-        if (!this.isOperator(this._operation[i])){
+        if (this.isOperator(this._operation[i]) == isOperator){
 
-            lastNumber = this._operation[i];
+            lastItem = this._operation[i];
             break;
         }
-    }
+   } 
+
+   if (!lastItem){
+
+    lastItem = (isOperator) ? this._lastOperator : this.lastNumber;
+
+   }
+
+  return lastItem;
+
+}
+
+  setLastNumberToDisplay(){
+
+    let lastNumber = this.getLastItem(false);
+
 
     if (!lastNumber) lastNumber = 0;
         this.displayCalc = lastNumber;
